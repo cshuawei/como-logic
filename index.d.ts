2b4f0c43b8bacc3139986d6ca8641bb6180960ef
COMO <cshuawei@vip.qq.com>
2021-11-24 15:11:19 +0800

    类型定义修改

--- index.d.ts
+++ index.d.ts
@@ -6,6 +6,6 @@ declare namespace Logic {
       children?: string;
     }
     export type Items = Array<{ [prop: string]: any }>
-    export type Return = (items: Items, parentId?: Id) => string[] | undefined
+    export type Return = (items: Items, parentId?: Id) => string[]
   }
 }
